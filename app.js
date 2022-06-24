6ba1edd6e4f0034b5b090b113b8c8e3edbefadf9
MaksonGitHub25 <makson94021@gmail.com>
2022-06-24 15:57:08 +0300

    register task complete, hurrah

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 alert('Привет, пользователь');
 
+let allUserList = [];
+
 const startFuncbtn = document.querySelector('.btn');
 
 function start() {
     userFunctionChoose = prompt('Выбери, что будет делать: \na) Зарегистрироваться\nб) Авторизироваться\nв) Просмотреть список всех пользователей\nг) Изменить данные пользователя\nд) Выйти');
 
     if (userFunctionChoose == 'а' || userFunctionChoose == 'Зарегистрироваться') {
-        allUserList = register();
+        alert('Чтоб пройти регистрацию, укажите все данные, которые потребуются далее');
+        register();
+
     } else if (userFunctionChoose == 'б' || userFunctionChoose == 'Авторизироваться') {
-        
+        logIn();
     } else if (userFunctionChoose == 'в' || userFunctionChoose == 'Просмотреть список всех пользователей') {
         
     } else if (userFunctionChoose == 'г' || userFunctionChoose == 'Изменить данные пользователя') {
@@ -22,14 +26,12 @@ function start() {
 }
 
 function register() {
-    let allUserList = [];
-    let userObj = {};
-    alert('Чтоб пройти регистрацию, укажите все данные, которые потребуются далее');
-    while (true) {
-        userName = prompt('Введите ваше имя');
-        userSurname = prompt('Введите вашу фамилию');
+    const userObj = {};
 
-        function askUserAge(){
+    userName = prompt('Введите ваше имя');
+    userSurname = prompt('Введите вашу фамилию');
+
+    function askUserAge(){
             userAge = +prompt('Введите ваш возраст');
 
             if (isNaN(userAge) || userAge == 0) {
@@ -37,54 +39,54 @@ function register() {
                 askUserAge();
             }
             return userAge;
-        }
-        userAge = askUserAge();
+    }
+    userAge = askUserAge();
 
-        function askUserEmail() {
+    function askUserEmail() {
             userEmail = prompt('Введите ваш email');
             if (userEmail.indexOf('@') == -1) {
                 alert('Ты ввел некорректный email!');
                 askUserEmail();
             }
             return userEmail;
-        }
-        userEmail = askUserEmail();
-
-        function askUserPassword() {
-            userPassword = prompt('Введите ваш пароль');
-            if (userPassword.length < 8){
-                alert('Ты ввел некорректный пароль! Пароль должен быть минимум 8 символов');
-                askUserPassword();
-            }
-            return userPassword;
-        }
-        userPassword = askUserPassword();
-
-        userObj['name'] = userName;
-        userObj['surname'] = userSurname;
-        userObj['age'] = userAge;
-        userObj['email'] = userEmail;
-        userObj['password'] = userPassword;
-
-        console.log(userObj);
-
-        allUserList.push(userObj);
-
-        isRepeat = confirm('Добавим ещё одного пользователя?');
-
-        if (isRepeat == true) {
-            register();
-        } else {
-            break;
-        }
     }
+    userEmail = askUserEmail();
+
+    function askUserPassword() {
+        userPassword = prompt('Введите ваш пароль');
+        if (userPassword.length < 8){
+            alert('Ты ввел некорректный пароль! Пароль должен быть минимум 8 символов');
+            askUserPassword();
+        }
+        return userPassword;
+    }
+    userPassword = askUserPassword();
+
+    userObj['name'] = userName;
+    userObj['surname'] = userSurname;
+    userObj['age'] = userAge;
+    userObj['email'] = userEmail;
+    userObj['password'] = userPassword;
+    
+    console.log(userObj);
+    
+    allUserList.push(userObj);
     console.log(allUserList);
 
-    return allUserList;
+    isRepeatReg();    
+}
+
+function isRepeatReg() {
+    isRepeat = confirm('Добавим ещё одного пользователя?');
+    if (isRepeat == true) {
+        register();
+    } else {
+        start();
+    }
 }
 
 function logIn() {
-
+    console.log(allUserList);
 }
 
 function checkAllUser() {
